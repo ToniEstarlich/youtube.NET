18c3f58c670b46fdfc019d2f7cc8d76268a1a1e0
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-11-12 22:54:02 +0000

    Add files via upload

---
+++ youtube.Music/myscript.js
@@ -0,0 +1,36 @@
+﻿// Function to start the animation
+function startAnimation() {
+    const titleElement = document.querySelector('.title');
+
+    // Check if the element with the class 'title' exists
+    if (!titleElement) {
+        console.error('Element with class "title" not found');
+        return;
+    }
+
+    let fontSize = 24;
+    let color = 0;
+
+    function animate() {
+        fontSize += 1;
+        color += 5;
+
+        // Check if titleElement still exists
+        if (titleElement) {
+            titleElement.style.fontSize = `${fontSize}px`;
+            titleElement.style.color = `rgb(0, 0, ${color})`;
+
+            if (fontSize < 36) {
+                requestAnimationFrame(animate);
+            }
+        }
+    }
+
+    animate();
+}
+
+// Ensure the DOM is fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function () {
+    // Trigger the animation after a delay
+    setTimeout(startAnimation, 1000);
+});
